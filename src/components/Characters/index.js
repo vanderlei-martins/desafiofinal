43e5029a8a7227897ecce383e109a4a9b65132de
Priscila <priscila.machado@superlogica.com>
2021-09-24 14:43:38 -0300

    add title

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,95 +1,98 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground, Image } from "react-native";
 import {
-	SafeArea,
-	Container,
-	ListCharacters,
-	Background,
-	SearchView,
-	SearchTextInput,
+  SafeArea,
+  Container,
+  ListCharacters,
+  Background,
+  SearchView,
+  SearchTextInput,
 } from "./styles";
 import { useSelector, useDispatch } from "react-redux";
 
 import {
-	getListAllCharacters,
-	getAllCharactersSelector,
+  getListAllCharacters,
+  getAllCharactersSelector,
 } from "../../redux/CharactersAction";
 
 import Card from "./Card";
 
 export default function Characters() {
-	const [page, setPage] = useState(1);
-	const dispatch = useDispatch();
-	const listCharacters = useSelector(getAllCharactersSelector);
-	const [charactersByFilter, setCharactersByFilter] = useState([]);
+  const [page, setPage] = useState(1);
+  const dispatch = useDispatch();
+  const listCharacters = useSelector(getAllCharactersSelector);
+  const [charactersByFilter, setCharactersByFilter] = useState([]);
 
-	useEffect(() => {
-		dispatch(getListAllCharacters());
-	}, []);
+  useEffect(() => {
+    dispatch(getListAllCharacters());
+  }, []);
 
-	async function getMoreCharacters() {
-		let nextPage = page + 1;
-		await setPage(nextPage);
-		await dispatch(getListAllCharacters(nextPage));
-	}
+  async function getMoreCharacters() {
+    let nextPage = page + 1;
+    await setPage(nextPage);
+    await dispatch(getListAllCharacters(nextPage));
+  }
 
-	function filterCharacters(text) {
-		if (!text) {
-			setCharactersByFilter([]);
-			return;
-		}
+  function filterCharacters(text) {
+    if (!text) {
+      setCharactersByFilter([]);
+      return;
+    }
 
-		let filteredCharacteres = listCharacters.filter((character) => {
-			let pos = character.name.toLowerCase().indexOf(text.toLowerCase());
-			return pos != -1;
-		});
+    let filteredCharacteres = listCharacters.filter((character) => {
+      let pos = character.name.toLowerCase().indexOf(text.toLowerCase());
+      return pos != -1;
+    });
 
-		setCharactersByFilter(filteredCharacteres);
-	}
+    setCharactersByFilter(filteredCharacteres);
+  }
 
-	function renderCharacter() {
-		return charactersByFilter.length !== 0
-			? charactersByFilter
-			: listCharacters;
-	}
+  function renderCharacter() {
+    return charactersByFilter.length !== 0
+      ? charactersByFilter
+      : listCharacters;
+  }
 
-	return (
-		<ImageBackground
-			source={require("../../img/teste4.jpg")}
-			resizeMode="cover"
-			style={styles.image}
-		>
-			<SafeArea>
-				<Container>
-					<SearchView>
-						<SearchTextInput
-							autoCapitalize="none"
-							autoCorrect={false}
-							clearButtonMode="always"
-							onChangeText={(text) => filterCharacters(text)}
-							placeholder="Digite o nome do personagem"
-						/>
-					</SearchView>
-					<Background>
-						<ListCharacters
-							showsVerticalScrollIndicator={false}
-							data={renderCharacter()}
-							keyExtractor={(item) => item.id}
-							renderItem={({ item }) => {
-								return <Card data={item} />;
-							}}
-							onEndReached={getMoreCharacters}
-							onEndReachedThreshold={0.1}
-						/>
-					</Background>
-				</Container>
-			</SafeArea>
-		</ImageBackground>
-	);
+  return (
+    <ImageBackground
+      source={require("../../img/teste4.jpg")}
+      resizeMode="cover"
+      style={styles.image}
+    >
+      <SafeArea>
+        <Container>
+          <Image
+            style={{ width: 308, height: 100, border: 2 }}
+            source={require("../../img/title1.png")}
+          />
+          <SearchView>
+            <SearchTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              clearButtonMode="always"
+              onChangeText={(text) => filterCharacters(text)}
+              placeholder="Digite o nome do personagem"
+            />
+          </SearchView>
+
+          <ListCharacters
+            showsVerticalScrollIndicator={false}
+            data={renderCharacter()}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              return <Card data={item} />;
+            }}
+            onEndReached={getMoreCharacters}
+            onEndReachedThreshold={0.1}
+          />
+        </Container>
+      </SafeArea>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-	image: {
-		flex: 1,
-	},
+  image: {
+    flex: 1,
+  },
 });
